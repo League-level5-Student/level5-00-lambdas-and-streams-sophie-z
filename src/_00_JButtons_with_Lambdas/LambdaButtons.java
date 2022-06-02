1e13670b4148865e06b5c6b40720c9e6caa2a5e9
sophie-z <sophie.zeng2@gmail.com>
2022-06-01 19:46:08 -0700

    finished

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,13 +1,16 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
-	private JButton addNumbers = new JButton("ADD 2 Numbers");
+	private JButton addNumbers = new JButton("ADD 2 NUMBERS??");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
 	
@@ -17,10 +20,31 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
-		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println();
+			}	
+		});
+		randNumber.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				Random random = new Random();
+				int randomInt = random.nextInt(1000001);
+				System.out.println(randomInt);
+			}	
+		});
+		tellAJoke.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("Where do boats go when they're sick?\nTHE DOC(K)");
+			}	
+		});
 		window.setVisible(true);
 		window.pack();
 	}
@@ -28,6 +52,5 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
-	
 	
 }
